63c7a2cb9cde9326fc8cefba9d06d2bae89a550e
Samar Omar <samaromm98@gmail.com>
2020-02-03 19:51:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Add your functions here
+
+function map(arr,fun){
+  fun(arr)
+}
+
+function toNegative(arr){
+  let arr2=[]
+  for(let ele of arr){
+    arr2.push(ele*-1)
+  }
+}
+
+map(toNegative)
