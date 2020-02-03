93c6c5f8eab6492451374e831db7ee3f0e4e144d
Samar Omar <samaromm98@gmail.com>
2020-02-03 20:08:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+
+function map(arr,fun){
+ let arr2=[]
+ for(let ele of arr){
+   arr2.push(fun(ele))
+ }
+ return arr2
+}
+
+function reduce(arr,fun, start=0){
+  let result=start
+  if (typeof fun(true, true) == 'boolean') {
+      result = true;
+    }
+  for(let a of arr){
+   result=fun(result,a)
+ }
+ return result
+}
